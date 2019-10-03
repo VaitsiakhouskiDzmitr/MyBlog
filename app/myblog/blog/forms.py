8d2dcb149a17ebb088ad3598a8c200057db06e8e
VaitsiakhouskiDzmitr <dzmitryvaitsiakhouski1995@gmail.com>
2019-10-03 21:47:03 +0200

    full form create , slug update

--- app/myblog/blog/forms.py
+++ app/myblog/blog/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+from .models import Post
+from django.core.exceptions import ValidationError
 
-class PostForm(Forms.ModelForm):
+class PostForm(forms.ModelForm):
     class Meta:
-        modek = Post
+        model = Post
         fields = ['title', 'slug', 'body']
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        return new_slug
--- app/myblog/blog/views.py
+++ app/myblog/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from .models import Post
 from django.views.generic import View
+from .forms import PostForm
 
 
 
@@ -16,6 +18,18 @@ class PostDetail(View):
     def get(self, request, slug):
         post = get_object_or_404(Post, slug__iexact=slug)
         return render(request, 'blog/post_detail.html', context={'post' : post})
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
 
 
 
